d0016173eb078f2d9e02d296b41875eb994cf751
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-06-21 23:35:38 +0200

    Add dummy scene to test unity versioning configuration

---
+++ Assets/SuperMegaPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializePrivateVariables]
+public class SuperMegaPlayer : MonoBehaviour {
+    
+    private float speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+    }
+}
